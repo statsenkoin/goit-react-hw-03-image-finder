9a4ae2d81f0a1b388c1b68bc113493035e2bf15d
Ihor Statsenko <statsenkoin@gmail.com>
2023-02-27 22:24:25 +0200

    Create Searchbar.jsx

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Searchbar = () => {
+  return (
+    <header className="searchbar">
+      <form className="form">
+        <button type="submit" className="button">
+          <span className="button-label">Search</span>
+        </button>
+
+        <input
+          className="input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+};
