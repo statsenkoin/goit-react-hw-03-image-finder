aa8d65cc84eae345c96dd91fe99b807442007d74
Ihor Statsenko <statsenkoin@gmail.com>
2023-02-27 22:57:27 +0200

    Feature/ add initial Formik form

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,22 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Formik, Form, Field } from 'formik';
 
 export const Searchbar = () => {
   return (
-    <header className="searchbar">
-      <form className="form">
-        <button type="submit" className="button">
-          <span className="button-label">Search</span>
-        </button>
-
-        <input
-          className="input"
-          type="text"
-          autoComplete="off"
-          autoFocus
-          placeholder="Search images and photos"
-        />
-      </form>
+    <header>
+      <Formik initialValues={{ input: '' }} onSubmit={(values, actions) => {}}>
+        <Form>
+          <button type="submit">
+            <span>Search</span>
+          </button>
+          <Field
+            type="text"
+            name="input"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          ></Field>
+        </Form>
+      </Formik>
     </header>
   );
+
+  //   return (
+  //     <header className="searchbar">
+  //       <form className="form">
+  //         <button type="submit" className="button">
+  //           <span className="button-label">Search</span>
+  //         </button>
+
+  //         <input
+  //           className="input"
+  //           type="text"
+  //           autoComplete="off"
+  //           autoFocus
+  //           placeholder="Search images and photos"
+  //         />
+  //       </form>
+  //     </header>
+  //   );
 };
