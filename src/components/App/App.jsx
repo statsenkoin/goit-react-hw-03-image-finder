3be6bf6b501e7eac3badc6f75f4cd51fadb15d9b
Ihor Statsenko <statsenkoin@gmail.com>
2023-02-27 22:24:11 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,5 @@
+import { Searchbar } from 'components';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <Searchbar></Searchbar>;
 };
