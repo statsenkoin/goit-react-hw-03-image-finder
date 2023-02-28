ff0010dc2d227e2624d63fc40c598db0d58a40a4
Ihor Statsenko <statsenkoin@gmail.com>
2023-02-28 17:17:32 +0200

    Update pixabayApi.js

--- src/utils/pixabayApi.js
+++ src/utils/pixabayApi.js
@@ -1,36 +1,32 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// const API_KEY = '32468715-2ee7d1cef437ed65ce73ff92a';
+const API_KEY = '32468715-2ee7d1cef437ed65ce73ff92a';
 // const BASE_URL = 'https://pixabay.com/api/';
-
-// const params = new URLSearchParams({
-//   key: API_KEY,
-//   image_type: 'photo',
-//   orientation: 'horizontal',
-//   safesearch: true,
-// });
-
-// export default async function fetchPixabay(query, page, perPage) {
-//   return await axios.get(
-//     `${BASE_URL}?q=${query}&page=${page}&per_page=${perPage}`,
-//     { params }
-//   );
-// }
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const params = new URLSearchParams({
+  key: API_KEY,
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  per_page: 12,
+});
 
 /**
  *
  * @param {String} query - userInput
  * @param {Number} page - requested page
- * @param {Number} perPage - number of requested objects
  * @returns array of objects or error message
+ * response.data = {total: 1159760, totalHits: 500, hits: Array(12)}
  */
-// export default async function fetchPixabay(query, page, perPage) {
-//   try {
-//     const responce = await fetch(
-//       `${BASE_URL}?q=${query}&page=${page}&per_page=${perPage}&${params}`
-//     );
-//     return await responce.json();
-//   } catch (error) {
-//     return console.log('error :>> ', error);
-//   }
+export default async function fetchPixabay(query, page) {
+  const response = await axios.get(`/?q=${query}&page=${page}`, {
+    params,
+  });
+  return response.data.hits;
+}
+// export default async function fetchPixabay(query, page) {
+//   const response = await axios.get(`${BASE_URL}?q=${query}&page=${page}`, {
+//     params,
+//   });
+//   return response.data.hits;
 // }
