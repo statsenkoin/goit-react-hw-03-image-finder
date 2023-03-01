8a969f7b275d96df7f9ce8fdae876283cfce980d
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-01 23:09:29 +0200

    Feature/ add ImageGallery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,12 +1,42 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { Gallery } from './ImageGallery.styled';
+import { ImageGalleryItem } from 'components';
 
-export class ImageGallery extends Component {
+import fetchPixabay from 'services/fetchPixabay';
+
+export class ImageGallery extends PureComponent {
   state = {
     page: 1,
+    gallery: null,
+    error: null,
   };
+
+  async componentDidUpdate(prevProps, prevState) {
+    const { page } = this.state;
+    const { userInput } = this.props;
+    if (prevProps.userInput !== userInput) {
+      const pictureSet = await fetchPixabay(userInput, page);
+      pictureSet.length
+        ? this.setState({ gallery: pictureSet, error: null })
+        : this.setState({ error: 'Not valid search query' });
+      // TODO toast
+    }
+  }
+
   render() {
+    const { gallery } = this.state;
+
     return (
-      <ul className="gallery">{/* <!-- Набір <li> із зображеннями --> */}</ul>
+      gallery && (
+        <Gallery>
+          <ImageGalleryItem gallery={gallery}></ImageGalleryItem>
+        </Gallery>
+      )
     );
   }
 }
+
+ImageGallery.propTypes = {
+  userInput: PropTypes.string.isRequired,
+};
