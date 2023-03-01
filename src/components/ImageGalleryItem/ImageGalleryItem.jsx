ddc028ad85eeffb60021f1d90b6b0b4f11c2449a
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-01 20:23:20 +0200

    Create ImageGalleryItem.jsx

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const ImageGalleryItem = () => {
+  return (
+    <li class="gallery-item">
+      <img src="" alt="" />
+    </li>
+  );
+};
+
+// export class ImageGalleryItem extends Component {
+//   state = {
+//     page: 1,
+//   };
+//   render() {
+//     return (
+//       <li class="gallery-item">
+//         <img src="" alt="" />
+//       </li>
+//     );
+//   }
+// }
