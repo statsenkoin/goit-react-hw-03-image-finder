9fa094466ffa0e45de82ae1d6917868207322166
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-01 23:10:32 +0200

    Feature/ add markup of gallery list

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { GalleryItem, GalleryItemImage } from './ImageGalleryItem.styled';
 
-export const ImageGalleryItem = () => {
-  return (
-    <li class="gallery-item">
-      <img src="" alt="" />
-    </li>
-  );
+export const ImageGalleryItem = ({ gallery }) => {
+  return gallery.map(({ id, webformatURL, tags }) => (
+    <GalleryItem key={id}>
+      <GalleryItemImage id={id} src={webformatURL} alt={tags} />
+    </GalleryItem>
+  ));
 };
 
-// export class ImageGalleryItem extends Component {
-//   state = {
-//     page: 1,
-//   };
-//   render() {
-//     return (
-//       <li class="gallery-item">
-//         <img src="" alt="" />
-//       </li>
-//     );
-//   }
-// }
+ImageGalleryItem.propTypes = {
+  gallery: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      webformatURL: PropTypes.string.isRequired,
+      tags: PropTypes.string.isRequired,
+    })
+  ),
+};
