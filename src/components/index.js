438ad9a059b82d733d4dda873eb5764f606338a1
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-04 11:55:13 +0200

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export * from './App/App';
 export * from './Searchbar/Searchbar';
 export * from './ImageGallery/ImageGallery';
 export * from './ImageGalleryItem/ImageGalleryItem';
+export * from './Modal/Modal';
