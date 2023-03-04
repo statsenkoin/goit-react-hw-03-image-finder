342a085ab172be9d68208cf34e275b4dc1fbd86b
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-05 00:00:11 +0200

    Feature/ add logic to show modal, refactor componentDidUpdate

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,62 +1,69 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Gallery, LoadMoreButton } from './ImageGallery.styled';
-import { ImageGalleryItem } from 'components';
-
 import fetchPixabay from 'services/fetchPixabay';
 import { pixabayConstants } from 'constants';
+import { Gallery } from './ImageGallery.styled';
+import { ImageGalleryItem, Modal, LoadMoreButton, Loader } from 'components';
+import { toast } from 'react-toastify';
 
 const { PER_PAGE } = pixabayConstants;
 
 export class ImageGallery extends Component {
   state = {
     page: 1,
-    pages: 1,
     gallery: null,
     error: null,
-    loading: false,
+    isLastPage: true,
+    isLoading: false,
+    isModalShown: false,
+    choosenImage: null,
   };
 
-  async componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(prevProps, prevState) {
     const { page, gallery } = this.state;
     const { input } = this.props;
     const { input: prevInput } = prevProps;
-    const { page: prevPage } = prevState;
+    const { page: prevPage, gallery: prevGallery } = prevState;
 
     if (prevInput !== input) {
       this.setState({ page: 1 });
     }
 
     if (prevInput !== input || prevPage !== page) {
-      try {
-        this.setState({ loading: true });
-
-        // data = {total: 1159760, totalHits: 500, hits: Array(12)}
-        const data = await fetchPixabay(input, page);
-        const { hits, total } = data;
-
-        if (!hits.length) {
-          this.setState({ error: 'Not valid search query' });
-          // TODO toast
-          return;
-        }
-        const pages = this.calculateTotalPages(total);
-
-        this.setState({
-          pages,
-          gallery:
-            gallery !== null && page > 1 ? [...gallery, ...hits] : [...hits],
-          error: null,
-        });
-      } catch (error) {
-        console.log('error :>> ', error);
-        this.setState({ error: 'Something went wrong. Try again later.' });
-        // TODO toast
-      } finally {
-        this.setState({ loading: false });
-      }
+      this.getImageSet();
     }
+
+    if (gallery !== prevGallery && page > 1) window.scrollBy(0, 450);
   }
+
+  getImageSet = async () => {
+    const { page, gallery } = this.state;
+    const { input } = this.props;
+
+    this.setState({ isLoading: true });
+    try {
+      // data = {total: 1159760, totalHits: 500, hits: Array(12)}
+      const { hits, total } = await fetchPixabay(input, page);
+
+      if (!hits.length) {
+        this.setState({ gallery: null });
+        toast.error('Not valid search query. Try another request.');
+        return;
+      }
+      const pages = this.calculateTotalPages(total);
+      this.setState({
+        gallery:
+          gallery !== null && page > 1 ? [...gallery, ...hits] : [...hits],
+        isLastPage: page < pages ? false : true,
+      });
+    } catch (error) {
+      console.log('error :>> ', error);
+      this.setState({ error: error.message, gallery: null });
+      toast.error('Something went wrong. Try again later.');
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
 
   calculateTotalPages = total => {
     return Math.ceil(total / PER_PAGE);
@@ -64,27 +71,42 @@ export class ImageGallery extends Component {
 
   onLoadMoreClick = () => {
     this.setState(({ page }) => ({ page: page + 1 }));
+  };
 
-    //   const newPage = this.state.page + 1;
-    //   this.setState({ page: newPage });
+  toggleModal = () => {
+    this.setState(({ isModalShown }) => ({
+      isModalShown: !isModalShown,
+    }));
+  };
+
+  onLargeImageShow = e => {
+    if (e.target.nodeName === 'IMG') {
+      const image = this.state.gallery.find(
+        item => item.id.toString() === e.target.id
+      );
+      this.setState({ choosenImage: image });
+      this.toggleModal();
+    }
   };
 
   render() {
-    const { gallery, page, pages, loading } = this.state;
+    const { gallery, isLoading, isModalShown, choosenImage, isLastPage } =
+      this.state;
     return (
       <>
+        {isLoading && <Loader />}
         {gallery && (
           <>
-            <Gallery>
+            <Gallery onClick={this.onLargeImageShow}>
               <ImageGalleryItem gallery={gallery}></ImageGalleryItem>
             </Gallery>
-            {loading && <p>Loading...</p>}
-            {page < pages && (
-              <LoadMoreButton type="button" onClick={this.onLoadMoreClick}>
-                Load more...
-              </LoadMoreButton>
-            )}
+            {!isLastPage && <LoadMoreButton onClick={this.onLoadMoreClick} />}
           </>
+        )}
+        {isModalShown && (
+          <Modal onClose={this.toggleModal}>
+            <img src={choosenImage.largeImageURL} alt={choosenImage.tags} />
+          </Modal>
         )}
       </>
     );
