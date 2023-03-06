f5f4cd895c4c26872e5c3e3941d7dc54b11394be
Ihor Statsenko <statsenkoin@gmail.com>
2023-03-06 13:48:21 +0200

    Fix/ gallery state moved to App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,99 @@
 import React, { Component } from 'react';
-import { Layout } from './App.styled';
-import { Searchbar, ImageGallery, Tostify } from 'components';
+import fetchPixabay from 'services/fetchPixabay';
+import { pixabayConstants } from 'constants';
+// import { toast } from 'react-toastify';
 
-import { Modal } from 'components';
+import { Layout } from './App.styled';
+import {
+  Searchbar,
+  ImageGallery,
+  Tostify,
+  LoadMoreButton,
+  Loader,
+  WarningPage,
+  Modal,
+} from 'components';
 
 class App extends Component {
   state = {
     input: '',
+    gallery: [],
+    page: 1,
+    isEmpty: false,
+    isLastPage: true,
+    isLoading: false,
     selectedImageUrl: null,
     selectedImageTags: null,
-    isModalShown: false,
+  };
+
+  componentDidUpdate(_, prevState) {
+    const { page, gallery, input } = this.state;
+    const {
+      input: prevInput,
+      page: prevPage,
+      gallery: prevGallery,
+    } = prevState;
+
+    if (prevInput !== input) this.resetState();
+
+    if (prevInput !== input || prevPage !== page) this.getImageSet();
+
+    if (gallery !== prevGallery && page > 1)
+      window.scrollBy(0, window.innerHeight / 2);
+  }
+
+  resetState = () => {
+    this.setState({
+      gallery: [],
+      page: 1,
+      error: null,
+      isLastPage: true,
+      isLoading: false,
+      isModalShown: false,
+    });
+  };
+
+  getImageSet = async () => {
+    const { input, page } = this.state;
+
+    this.setState({ isLoading: true });
+    try {
+      // data = {total: 1159760, totalHits: 500, hits: Array(12)}
+      const { hits, total } = await fetchPixabay(input, page);
+
+      if (hits.length === 0) {
+        // return toast.error('Not valid search query. Try another request.');
+        return this.setState({ isEmpty: true });
+      }
+
+      const pages = this.calculateTotalPages(total);
+      this.setState(prev => ({
+        gallery: [...prev.gallery, ...hits],
+        isLastPage: page < pages ? false : true,
+        error: null,
+        isEmpty: false,
+      }));
+    } catch (error) {
+      console.log('error :>> ', error);
+      this.setState({ error: error.message });
+      // toast.error('Something went wrong. Try again later.');
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  calculateTotalPages = total => {
+    const { PER_PAGE } = pixabayConstants;
+    return Math.ceil(total / PER_PAGE);
   };
 
   handleSearchInput = userInput => {
     const { input } = this.state;
     if (input !== userInput) this.setState({ input: userInput });
+  };
+
+  onLoadMoreClick = () => {
+    this.setState(({ page }) => ({ page: page + 1 }));
   };
 
   selectModalImage = (link, tags) => {
@@ -29,15 +108,38 @@ class App extends Component {
   };
 
   render() {
-    const { input, selectedImageUrl, selectedImageTags, isModalShown } =
-      this.state;
+    const {
+      selectedImageUrl,
+      selectedImageTags,
+      isModalShown,
+      isLoading,
+      isLastPage,
+      isEmpty,
+      error,
+      gallery,
+    } = this.state;
     return (
       <Layout>
+        {isLoading && <Loader />}
+
         <Searchbar handleSearchInput={this.handleSearchInput}></Searchbar>
-        <ImageGallery
-          input={input}
-          onSelectModalImage={this.selectModalImage}
-        ></ImageGallery>
+
+        {gallery && (
+          <>
+            <ImageGallery
+              onSelectModalImage={this.selectModalImage}
+              gallery={gallery}
+            ></ImageGallery>
+            {!isLastPage && <LoadMoreButton onClick={this.onLoadMoreClick} />}
+          </>
+        )}
+
+        {isEmpty && <WarningPage text={'There is nothing to show.'} />}
+        {error && (
+          <WarningPage text={`Something went wrong.\n Try again later.`} />
+          // <WarningPage text={error} />
+        )}
+
         <Tostify />
 
         {isModalShown && (
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -20,6 +20,7 @@ ImageGalleryItem.propTypes = {
     PropTypes.shape({
       id: PropTypes.number.isRequired,
       webformatURL: PropTypes.string.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
       tags: PropTypes.string.isRequired,
     })
   ),
